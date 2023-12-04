1ae54f0a9323e03b15eec5ee47be79fa8004ecc2
Tom Zhang <tommyawesome08@gmail.com>
2023-12-04 16:54:53 -0500

    added permissions - only staff can change articles

--- engineering_cornerstone/reading_hub/views.py
+++ engineering_cornerstone/reading_hub/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
+from .permissions import *
 
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = ArticleSerializer
 
 
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = ArticleSerializer

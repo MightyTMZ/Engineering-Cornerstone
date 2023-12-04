779359de71f2f08184557159b38ef7ea797f9245
Tom Zhang <tommyawesome08@gmail.com>
2023-12-03 20:49:44 -0500

    changed the admin site header

--- engineering_cornerstone/engineering_cornerstone/urls.py
+++ engineering_cornerstone/engineering_cornerstone/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import debug_toolbar
+
+admin.site.site_header = "Engineering Cornerstone Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__debug__", include(debug_toolbar.urls))
 ]

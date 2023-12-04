cce2527b6c9a5d1a6624ec3cb1e35d1e8494341a
Tom Zhang <tommyawesome08@gmail.com>
2023-12-03 20:47:42 -0500

    set up the finalized schema for reading hub app

--- engineering_cornerstone/reading_hub/models.py
+++ engineering_cornerstone/reading_hub/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 from users.models import CustomUser
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.user.first_name + self.user.last_name
+    
+
 class Article(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField()
     content = models.TextField()  # Store the article content as HTML or Markdown
-    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
+# url structure:
+# engineeringcornerstone.com/median_form(e.g. article, blog, etc)/created_at_date/slug
+# example url:
+# https://www.engineeringcornerstone.com/article/2023-12-03/django-is-a-python-backend-framework

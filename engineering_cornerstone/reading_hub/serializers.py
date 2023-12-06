f1fe79d0b3be99307eacf26dba21ab8f8ef6813c
Tom Zhang <tommyawesome08@gmail.com>
2023-12-06 18:07:42 -0500

    db schema back to efficient form for articles

--- engineering_cornerstone/reading_hub/serializers.py
+++ engineering_cornerstone/reading_hub/serializers.py
@@ -4,5 +4,5 @@ from .models import Article, Author, Category
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', "image_url", 'category', 'content', 'author', 'created_at', 'updated_at', 'trending']
+        fields = ['id', 'title', 'slug', "image_url", 'category', 'content', 'author', 'created_at', 'updated_at', 'trending']
 

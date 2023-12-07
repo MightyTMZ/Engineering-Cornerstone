cc740fbbdba152cf224516f720b11acea478ab5d
Tom Zhang <tommyawesome08@gmail.com>
2023-12-07 18:03:48 -0500

    finalized the author serializer

--- engineering_cornerstone/reading_hub/serializers.py
+++ engineering_cornerstone/reading_hub/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Article, Author, Category
 from users.serializers import *
 
-class AuthorSerializer(serializers.Serializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'first_name', 'last_name', 'profile_picture_url']
@@ -10,10 +10,10 @@ class AuthorSerializer(serializers.Serializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     created_at_date = serializers.DateField(read_only=True)
-    #last_updated_date = serializers.DateField(source="updated_at.date", read_only=True)
     authors = AuthorSerializer(many=True)
 
     class Meta:
         model = Article
-        fields = ['id', 'title', 'slug', 'image_url', 'category', 'content', 'author', 'created_at', 'created_at_date', 
+        fields = ['id', 'title', 'slug', 'image_url', 'category', 'content', 'authors', 'created_at', 'created_at_date', 
                   'updated_at', 'trending']
+

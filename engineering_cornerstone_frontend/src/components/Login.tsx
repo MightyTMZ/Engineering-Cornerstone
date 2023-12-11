0d1dc17de47363b3d635fe4e41fe0f87d9989d0f
Tom Zhang <tommyawesome08@gmail.com>
2023-12-11 08:35:58 -0500

    connected the backend with login form but not token yet

--- engineering_cornerstone_frontend/src/components/Login.tsx
+++ engineering_cornerstone_frontend/src/components/Login.tsx
@@ -1,43 +1,88 @@
-import NavBar from "./NavBar/NavBar"
+import NavBar from "./NavBar/NavBar";
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(username)
+    console.log(password)
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/auth/jwt/create/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Authentication failed");
+      }
+      else if (response.ok){
+        console.log("Authentication success")
+      }
+
+      // Handle successful authentication, e.g., redirect or store tokens
+
+    } catch (error) {
+      console.error("Login error:", error);
+      // Handle login error, e.g., display an error message to the user
+    }
+  };
+
   return (
     <div className="container">
-    <NavBar/>
-    <div className="container mt-5">
+      <NavBar />
+      <div className="container mt-5">
         <div className="row justify-content-center">
-            <div className="col-md-6">
-                <div className="card">
-                    <div className="card-header">
-                        <h4>Login</h4>
-                    </div>
-                    <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="username">Username or Email</label>
-                                <input type="text" className="form-control mt-1" id="username" placeholder="Username or email"/>
-                            </div>
-                            <div className="form-group mt-3">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control mt-1" id="password" placeholder="Password"/>
-                            </div>
-                            <button type="submit" className="btn btn-primary mt-4">Login</button>
-                        </form>
-                    </div>
-                </div>
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h4>Login</h4>
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                      type="text"
+                      className="form-control mt-1"
+                      id="username"
+                      placeholder="Username or email"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      className="form-control mt-1"
+                      id="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary mt-4">
+                    Login
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossOrigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossOrigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossOrigin="anonymous"></script>
-    </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
